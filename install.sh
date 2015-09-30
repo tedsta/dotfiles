2445a95ab9e7f9a4811bd65d5dae6ece2c81ab29
Theodore DeRego <tderego94@gmail.com>
2015-09-29 15:10:17 -1000

    ffmpeg installation

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 sudo apt-get install chromium-browser vim python3-pip git gcc
 
 # Some game dev stuff
-sudo apt-get install libfreetype6-dev libavutil-dev libsdl2-dev libavcodec-dev libavformat-dev libavfilter-dev libavdevice-dev libpostproc-dev libswscale-dev libavresample-dev
+sudo apt-get install libfreetype6-dev libsdl2-dev
+
+# FFmpeg
+sudo add-apt-repository ppa:kirillshkrogalev/ffmpeg-next
+sudo apt-get update
+sudo apt-get install libavutil-ffmpeg-dev libavcodec-ffmpeg-dev libavdevice-ffmpeg-dev libavformat-ffmpeg-dev libavfilter-ffmpeg-dev libpostproc-ffmpeg-dev libswresample-ffmpeg-dev libswscale-ffmpeg-dev
 
 # Install OpenCL
 sudo apt-get install cmake pkg-config python ocl-icd-dev ocl-icd-opencl-dev libdrm-dev libxfixes-dev libxext-dev llvm-3.5-dev clang-3.5 libclang-3.5-dev libtinfo-dev libedit-dev zlib1g-dev
